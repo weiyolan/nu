eaf7bf910cf5935e76cb0de253ddbec0c1c8d98a
weiyolan <yolan.weiler@gmail.com>
2024-01-27 23:28:57 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import Image from 'next/image'
+import Image from "next/image";
 // import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          L'utilisation d'un ordinateur est conseillé.
+          {"L'utilisation d'un ordinateur est conseillé."}
           {/* <code className="font-mono font-bold">app/page.tsx</code> */}
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
@@ -18,8 +18,9 @@ export default function Home() {
       <div className="relative flex flex-col place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert" src="/NU.svg" alt="NU Logo" width={180} height={37} priority />
         <p className={`mt-8 max-w-[45%] text-center font-light`}>
-          Ce site vous offre une simulation de design. Explorez les différentes pages de votre site web en utilisant les boutons ci-dessous. Veuillez noter que le contenu n'est pas
-          adaptatif à la taille de votre écran, donc ses proportions peuvent encore changer.
+          {
+            "Ce site vous offre une simulation de design. Explorez les différentes pages de votre site web en utilisant les boutons ci-dessous. Veuillez noter que le contenu n'est pas adaptatif à la taille de votre écran, donc ses proportions peuvent encore changer."
+          }
         </p>
       </div>
 
@@ -33,7 +34,7 @@ export default function Home() {
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Accueil <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">-&gt;</span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Une simulation de la page d'accueil.</p>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>{`Une simulation de la page d'accueil.`}</p>
         </a>
 
         <a
@@ -69,7 +70,7 @@ export default function Home() {
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Produit <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">-&gt;</span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Une page détaillée d'un produit.</p>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>{`Une page détaillée d'un produit.`}</p>
         </a>
         <a
           href="./article"
@@ -80,7 +81,7 @@ export default function Home() {
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Article <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">-&gt;</span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Une simulation d'un article de la Gazette Nu.</p>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>{`Une simulation d'un article de la Gazette Nu.`}</p>
         </a>
       </div>
     </main>
