d4472bf9fea02e606138574b6ac608ab455f6aaa
weiyolan <yolan.weiler@gmail.com>
2024-01-28 10:22:08 +0100

    responsive

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 // import Link from "next/link";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-12 pt-24 sm:p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           {"L'utilisation d'un ordinateur est conseillé."}
@@ -17,7 +17,7 @@ export default function Home() {
 
       <div className="relative flex flex-col place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert" src="/NU.svg" alt="NU Logo" width={180} height={37} priority />
-        <p className={`mt-8 md:max-w-[45%] text-center font-light`}>
+        <p className={`mt-8 w-full md:max-w-[45%] text-center font-light`}>
           {`Salut Lou! Ce site vous offre une simulation de design. Explorez les différentes pages de votre site web en utilisant les boutons ci-dessous. 
             Attention, le contenu n'est pas adaptatif à la taille de votre écran avec des proportions potentiellement sous-optimal. 
             Le chargement initial des pages individuelles peut être long, alors veuillez patientez. Profitez! `}
