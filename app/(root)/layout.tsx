8ff1a934cef63d5cf20183eafda56ced8b9f6825
weiyolan <yolan.weiler@gmail.com>
2024-02-12 23:28:15 +0100

    v2

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Alegreya_Sans, Inter, Mulish } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import Navbar from "@/components/navbar";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,16 +11,25 @@ export const metadata: Metadata = {
   description: "Shampoing solide à base de levure de bi",
 };
 
-const alegreya = Alegreya_Sans({ subsets: ["latin"], weight: ["300", "400", "700"] });
+// const alegreya = Alegreya_Sans({ subsets: ["latin"], weight: ["300", "400", "700"] });
 
 const mulish = Mulish({ subsets: ["latin"] });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  console.log("Root");
+  // console.log("Root");
 
   return (
     <html lang="en">
-      <body className={` ${alegreya.className} `}>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      <body className={` ${mulish.className} `}>
         {/* <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange> */}
         {/* <Navbar /> */}
         {children}
